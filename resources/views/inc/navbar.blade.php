cbbd83106b7df3ed12bc6988d60628a49dc6f95a
Luiz Schinemann <luizschinemann@gmail.com>
2018-05-08 15:58:38 -0300

    teste

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,7 +6,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
+                    <a class="nav-link" href="mocelinAdv/">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="quem-somos">Quem Somos</a>
