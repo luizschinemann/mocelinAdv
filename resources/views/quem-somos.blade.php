3abca77534bc412eabfdc182f2f4b183ada123ce
Luiz Schinemann <luizschinemann@gmail.com>
2018-05-11 18:18:18 -0300

    layout quem somos

--- resources/views/quem-somos.blade.php
+++ resources/views/quem-somos.blade.php
@@ -1,14 +1,18 @@
 @extends('layout.app')
 @section('content')
 <div class="container">
-    O Escritório de Advocacia Fernando Mocelin Moraes & Advogados presta serviços jurídicos com um padrão de excelência e elevado nível técnico nas áreas de Direito do Trabalho, Médico, Consumidor e Civil.
+    <div class="row">
+        <div class="col-9"></div>
+        <div class="col-3">O Escritório de Advocacia Fernando Mocelin Moraes & Advogados presta serviços jurídicos com um padrão de excelência e elevado nível técnico nas áreas de Direito do Trabalho, Médico, Consumidor e Civil.
+                
+                A relação com o cliente é um dos pontos fortes nos serviços prestados. Primamos pelo contato constante, transparência e flexibilidade na adequação de processos e suas necessidades, de forma a minimizar ou até mesmo eliminar a ocorrência de riscos e custos desnecessários aos clientes.
+                
+                A dinâmica de trabalho que adotamos é norteada pela parceria e pelo bom relacionamento com o cliente, a fim de desenvolver os processos dentro de um padrão que se orienta pela assertividade de soluções jurídicas.
+                
+                O diferencial dos nossos serviços está na atuação com foco em áreas jurídicas específicas, e esse expertise nos possibilita aplicar metodologias mais modernas e avançadas para cada área do Direito em que atuamos.
+                
+                O resultado é um alto padrão de qualidade à disposição do cliente que se traduz na condução dos serviços oferecidos em busca da melhor solução e conclusão dos processos.</div>
+    </div>
     
-    A relação com o cliente é um dos pontos fortes nos serviços prestados. Primamos pelo contato constante, transparência e flexibilidade na adequação de processos e suas necessidades, de forma a minimizar ou até mesmo eliminar a ocorrência de riscos e custos desnecessários aos clientes.
-    
-    A dinâmica de trabalho que adotamos é norteada pela parceria e pelo bom relacionamento com o cliente, a fim de desenvolver os processos dentro de um padrão que se orienta pela assertividade de soluções jurídicas.
-    
-    O diferencial dos nossos serviços está na atuação com foco em áreas jurídicas específicas, e esse expertise nos possibilita aplicar metodologias mais modernas e avançadas para cada área do Direito em que atuamos.
-    
-    O resultado é um alto padrão de qualidade à disposição do cliente que se traduz na condução dos serviços oferecidos em busca da melhor solução e conclusão dos processos.
 </div>
 @endsection
