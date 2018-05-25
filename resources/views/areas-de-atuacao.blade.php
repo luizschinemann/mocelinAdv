f6686780fe5e68ad7a8e5dc2bc1fc9d745ab2d89
Luiz Schinemann <luizschinemann@gmail.com>
2018-05-25 17:45:03 -0300

    diagramacao pagina de areas de atuacao

--- resources/views/areas-de-atuacao.blade.php
+++ resources/views/areas-de-atuacao.blade.php
@@ -14,94 +14,96 @@
         Consultoria para o Trabalhador
         
         Quanto à atuação em prol do trabalhador, nossa equipe está preparada para garantir os direitos próprios da classe, oriundos da relação de emprego: 
+        <ul>
+            <li>Acidente de trabalho;</li>
+            <li>Ações indenizatórias decorrentes de acidente de trabalho e doenças ocupacionais;</li>
+            <li>Questões relacionadas à admissão e demissão do empregado;</li>
+            <li>Aviso prévio proporcional;</li>
+            <li>Cálculo do valor correto da rescisão contratual;</li>
+            <li>CIPA;</li>
+            <li>Contratação de estrangeiro;</li>
+            <li>Cooperativas;</li>
+            <li>Desvio e acúmulo de função;</li>
+            <li>Dobras e horas-extras ilegais;</li>
+            <li>Empregado doméstico;</li>
+            <li>Equiparação e reenquadramento salarial;</li>
+            <li>Férias, décimo terceiro salário e aviso prévio;</li>
+            <li>Fundo de Garantia por Tempo de Serviço (FGTS);</li>
+            <li>Indenização por assédio moral e sexual;</li>
+            <li>Adicional de insalubridade e periculosidade;</li>
+            <li>Irregularidades em contrato de trabalho;</li>
+            <li>Demissão por justa causa;</li>
+            <li>Licença-maternidade e paternidade;</li>
+            <li>Multa por atraso de pagamento;</li>
+            <li>Reclamações trabalhistas em geral;</li>
+            <li>Reintegração do empregado;</li>
+            <li>Repouso semanal remunerado;</li>
+            <li>Rescisão;</li>
+            <li>Trabalho temporário;</li>
+            <li>Trabalho intermitente;</li>
+            <li>Teletrabalho;</li>
+            <li>Trabalho noturno;</li>
+            <li>Vale-transporte;</li>
+            <li>Integração de valores em remuneração.</li>
+        </ul>
         
-        Acidente de trabalho;
-        Ações indenizatórias decorrentes de acidente de trabalho e doenças ocupacionais;
-        Questões relacionadas à admissão e demissão do empregado;
-        Aviso prévio proporcional;
-        Cálculo do valor correto da rescisão contratual;
-        CIPA;
-        Contratação de estrangeiro;
-        Cooperativas;
-        Desvio e acúmulo de função;
-        Dobras e horas-extras ilegais;
-        Empregado doméstico;
-        Equiparação e reenquadramento salarial;
-        Férias, décimo terceiro salário e aviso prévio;
-        Fundo de Garantia por Tempo de Serviço (FGTS);
-        Indenização por assédio moral e sexual;
-        Adicional de insalubridade e periculosidade;
-        Irregularidades em contrato de trabalho;
-        Demissão por justa causa;
-        Licença-maternidade e paternidade;
-        Multa por atraso de pagamento;
-        Reclamações trabalhistas em geral;
-        Reintegração do empregado;
-        Repouso semanal remunerado;
-        Rescisão;
-        Trabalho temporário;
-        Trabalho intermitente;
-        Teletrabalho;
-        Trabalho noturno;
-        Vale-transporte;
-        Integração de valores em remuneração.
-         Consultoria para Empresas 
+        <b>Consultoria para Empresas</b> 
+        <ul>
+            <li>Contencioso Judicial Trabalhista;</li>
+            <li>Arbitragem, conciliação e Mediação;</li>
+            <li>Aspectos trabalhistas decorrentes de processo de aquisição, fusão e cisão e outras formas de organização empresarial;</li>
+            <li>Auditorias jurídico laborais e procedimentais;</li>
+            <li>Contratação coletiva e gestão de conflitos coletivos;</li>
+            <li>Criação de estrutura de representação coletiva dos trabalhadores, incluindo conselhos de empresa e comissões de trabalhadores;</li>
+            <li>Definição de planos de remuneração e benefícios;</li>
+            <li>Demissões individuais e coletivas, incluindo lay-off e extinção de postos de trabalho;</li>
+            <li>Direito de imigração, incluindo contratação e destacamento de trabalhadores estrangeiros não comunitários e processos de pedido de vistos de trabalho e outros;</li>
+            <li>Elaboração de contratos com vistas ao recrutamento de pessoal;</li>
+            <li>Equiparação salarial e paradigma trabalhista;</li>
+            <li>Horas-extras;</li>
+            <li>Organização do tempo de trabalho, regimes de adaptabilidade, bancos de horas e afins;</li>
+            <li>Procedimentos disciplinares;</li>
+            <li>Segurança social, incluindo regimes de antecipação de reforma e de pré-reforma;</li>
+            <li>Transferências temporárias e definitivas, individuais e coletivas, de locais de trabalho.</li>            
+        </ul>
         
-        Contencioso Judicial Trabalhista;
-        Arbitragem, conciliação e Mediação;
-        Aspectos trabalhistas decorrentes de processo de aquisição, fusão e cisão e outras formas de organização empresarial;
-        Auditorias jurídico laborais e procedimentais;
-        Contratação coletiva e gestão de conflitos coletivos;
-        Criação de estrutura de representação coletiva dos trabalhadores, incluindo conselhos de empresa e comissões de trabalhadores;
-        Definição de planos de remuneração e benefícios;
-        Demissões individuais e coletivas, incluindo lay-off e extinção de postos de trabalho;
-        Direito de imigração, incluindo contratação e destacamento de trabalhadores estrangeiros não comunitários e processos de pedido de vistos de trabalho e outros;
-        Elaboração de contratos com vistas ao recrutamento de pessoal;
-        Equiparação salarial e paradigma trabalhista;
-        Horas-extras;
-        Organização do tempo de trabalho, regimes de adaptabilidade, bancos de horas e afins;
-        Procedimentos disciplinares;
-        Segurança social, incluindo regimes de antecipação de reforma e de pré-reforma;
-        Transferências temporárias e definitivas, individuais e coletivas, de locais de trabalho.
-         DIREITO MÉDICO (HEALTH CARE)  
+        <b>DIREITO MÉDICO (HEALTH CARE)</b><br><br>  
         
-        Visa, principalmente, o aspecto jurídico no trabalho dos profissionais de saúde, especialmente médicos, além de entidades envolvidas na assistência médica.
-        
-        O Escritório Fernando Mocelin Moraes & Advogados oferece atendimento personalizado e eficiente, mantendo seu padrão de qualidade e assertividade nas questões litigiosas também na área do Direito da Saúde, destacando-se  demandas que incluem a defesa do cliente em Ações Judiciais envolvendo Erro Médico; Ações Indenizatórias de procedimentos cirúrgicos ou medicamentosos; Processos Administrativos, dentre outros.
-        
+        Visa, principalmente, o aspecto jurídico no trabalho dos profissionais de saúde, especialmente médicos, além de entidades envolvidas na assistência médica.<br>        
+        O Escritório Fernando Mocelin Moraes & Advogados oferece atendimento personalizado e eficiente, mantendo seu padrão de qualidade e assertividade nas questões litigiosas também na área do Direito da Saúde, destacando-se  demandas que incluem a defesa do cliente em Ações Judiciais envolvendo Erro Médico; Ações Indenizatórias de procedimentos cirúrgicos ou medicamentosos; Processos Administrativos, dentre outros.<br>        
         Ainda, prestamos consultoria para gestão jurídica de operadoras de saúde de pequeno e médio portes, de maneira a evitar passivos judiciais, em especial na elaboração de contratos que seguiam a essência do Código de Defesa do Consumidor e das Normas da ANS.
         
-        DIREITO DO CONSUMIDOR
+       <b>DIREITO DO CONSUMIDOR</b><br><br>
+
+        Nosso escritório é plenamente habilitado para lidar com conflitos de consumo e com a defesa dos direitos dos consumidores. Os serviços prestados por nossa equipe nessa área são:<br>
+        <ul>
+            <li>Defesa do consumidor, no que tange a problemas relativos à qualidade de produtos e serviços, prevenção e reparação de danos oriundos de relações de consumo, proteção da saúde e segurança do consumidor;</li>
+            <li>Questões oriundas de práticas comerciais como condutas abusivas, publicidade – ofertas de produtos –, direito de arrependimento, cobrança de dívidas, bancos de dados e cadastros de consumidores no SPC, SERASA, CCF;</li>
+            <li>Cobrança de dívidas de consumidores perante bancos, empresas de cartão de crédito e financeiras (renegociação de contratos);</li>
+            <li>Danos decorrentes de abalo de crédito por inscrição indevida em cadastros de inadimplentes;</li>
+            <li>Cláusulas abusivas existentes em contratos de adesão, de fornecimento de produtos e serviços das mais variadas espécies (planos de saúde, transporte, turismo, viagem, hospedagem, depósito, estacionamento, seguro, previdência privada, bancários, financiamento, administração de consórcios, fornecimento de serviços públicos, e demais);</li>
+            <li>Danos materiais e morais na relação de consumo, envolvendo compras feitas pela internet;  </li>
+            <li>Descumprimento de contratos de planos de saúde;</li>                  
+            <li>Defesas administrativas no PROCON.</li>
+        </ul>       
+        <b>DIREITO CIVIL</b>
         
-        Nosso escritório é plenamente habilitado para lidar com conflitos de consumo e com a defesa dos direitos dos consumidores. Os serviços prestados por nossa equipe nessa área são:
+        Esta é uma área do Direito em que são tratadas questões que regulam as relações jurídicas entre as pessoas físicas ou jurídicas, mantendo uma relação de equilíbrio e igualdade de condições.<br>
         
-        Defesa do consumidor, no que tange a problemas relativos à qualidade de produtos e serviços, prevenção e reparação de danos oriundos de relações de consumo, proteção da saúde e segurança do consumidor; 
-        Questões oriundas de práticas comerciais como condutas abusivas, publicidade – ofertas de produtos –, direito de arrependimento, cobrança de dívidas, bancos de dados e cadastros de consumidores no SPC, SERASA, CCF; 
-        Cobrança de dívidas de consumidores perante bancos, empresas de cartão de crédito e financeiras (renegociação de contratos); 
-        Danos decorrentes de abalo de crédito por inscrição indevida em cadastros de inadimplentes;  
-        Cláusulas abusivas existentes em contratos de adesão, de fornecimento de produtos e serviços das mais variadas espécies (planos de saúde, transporte, turismo, viagem, hospedagem, depósito, estacionamento, seguro, previdência privada, bancários, financiamento, administração de consórcios, fornecimento de serviços públicos, e demais);  
-        Danos materiais e morais na relação de consumo, envolvendo compras feitas pela internet;  
-        Descumprimento de contratos de planos de saúde;  
-        Defesas administrativas no PROCON.
-         
-        
-        DIREITO CIVIL
-        
-        Esta é uma área do Direito em que são tratadas questões que regulam as relações jurídicas entre as pessoas físicas ou jurídicas, mantendo uma relação de equilíbrio e igualdade de condições.
-        
-        Fazem parte dessas relações casamentos, divórcios e falecimentos (inventários e testamentos). Ainda, contratos de compra e venda, ações de despejo e indenizações. Atuamos nessa área prestando os seguintes serviços:  
-        
-        Assessoria, elaboração e revisão de contratos civis;
-        Ações possessórias;
-        Ações de despejo;
-        Indenizações;
-        Constituição de garantias de cumprimento de obrigações;
-        Danos morais;
-        Renegociação de contratos;
-        Cobrança de condomínio;
-        Recuperação de créditos;
-        Responsabilidade civil;
-        Ação anulatória;
-        Falência e recuperação judicial.
+        Fazem parte dessas relações casamentos, divórcios e falecimentos (inventários e testamentos). Ainda, contratos de compra e venda, ações de despejo e indenizações. Atuamos nessa área prestando os seguintes serviços:<br>  
+        <ul>
+            <li>Assessoria, elaboração e revisão de contratos civis;</li>
+            <li>Ações possessórias;</li>
+            <li>Ações de despejo;</li>
+            <li>Indenizações;</li>
+            <li>Constituição de garantias de cumprimento de obrigações;</li>
+            <li>Danos morais;</li>
+            <li>Renegociação de contratos;</li>
+            <li>Cobrança de condomínio;</li>
+            <li>Recuperação de créditos;</li>
+            <li>Responsabilidade civil;</li>
+            <li>Ação anulatória;</li>
+            <li>Falência e recuperação judicial.</li>       
+        </ul>
     </div>
 @endsection
