25d541cbb31cac64b82e66b2fb12ec31341f710d
Luiz Schinemann <luizschinemann@gmail.com>
2018-06-11 16:14:14 -0300

    Header

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 	<div class="row justify-content-md-center mt-4 mb-4 ">		
 		<div class="col-md-auto">
-			<center><h1>Sócio fundador</h1></center>	
+			<center><h1 class="border-bottom">Sócio fundador</h1></center>	
 			<a href="{{ url('fernando-mocelin-moraes') }}">
 				<img src="{{asset('public/img/user.png')}}" alt="FERNANDO HENRIQUE PIRES MOCELIN MORAES" class="mx-auto d-block">
 			</a>
