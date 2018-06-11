44882cd780a8302433429708e41823f21c47e6c1
Luiz Schinemann <luizschinemann@gmail.com>
2018-06-11 15:40:50 -0300

    Header

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -12,5 +12,15 @@
 			<h4><kbd>Fernando Henrique Pires Mocelin Moraes</kbd></h4>
 		</div>		
 	</div>	
+	<div class="row justify-content-md-center">		
+		<div class="col-md-auto">
+			<center><h1>Estagiário</h1></center>	
+			<a href="{{ url('fernando-mocelin-moraes') }}">
+				<img src="{{asset('public/img/user.png')}}" alt="RODRIGO CORREA NUNES" class="mx-auto d-block">
+			</a>
+			<br>
+			<h4><kbd>Rodrigo Correa Nunes</kbd></h4>
+		</div>		
+	</div>	
 </div>
 @endsection
