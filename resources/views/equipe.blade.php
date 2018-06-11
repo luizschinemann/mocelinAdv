ba031163e15a2090721bcc27be15424158ac22a1
Luiz Schinemann <luizschinemann@gmail.com>
2018-06-11 15:36:40 -0300

    Header

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -2,21 +2,15 @@
 @section('content')
 
 <div class="container">
-	<div class="row justify-content-md-center">
-		<div class="col col-lg-2">
-		1 of 3
-		</div>
+	<div class="row justify-content-md-center">		
 		<div class="col-md-auto">
-				<h1>Sócio fundador</h1>
-				<a href="{{ url('fernando-mocelin-moraes') }}">
-					<img src="{{asset('public/img/user.png')}}" alt="FERNANDO HENRIQUE PIRES MOCELIN MORAES" class="mx-auto d-block">
-				</a>
-				<br>
-				<h4><kbd>Henrique Pires Mocelin Moraes</kbd></h4>
-		</div>
-		<div class="col col-lg-2">
-		3 of 3
-		</div>
+			<h1>Sócio fundador</h1>
+			<a href="{{ url('fernando-mocelin-moraes') }}">
+				<img src="{{asset('public/img/user.png')}}" alt="FERNANDO HENRIQUE PIRES MOCELIN MORAES" class="mx-auto d-block">
+			</a>
+			<br>
+			<h4><kbd>Fernando Henrique Pires Mocelin Moraes</kbd></h4>
+		</div>		
 	</div>
 	<div class="d-flex justify-content-center">
 		<h1>Sócio fundador</h1>
