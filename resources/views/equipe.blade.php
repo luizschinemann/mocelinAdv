ecde10710c208fb554d7ec052e55662efceeb673
Luiz Schinemann <luizschinemann@gmail.com>
2018-06-11 15:18:42 -0300

    centralizando

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
 <div class="row">
-		<div class="d-flex justify-content-center">	
+		<div class="">	
 		<a href="{{ url('fernando-mocelin-moraes') }}">
 			<img src="{{asset('public/img/user.png')}}" alt="FERNANDO HENRIQUE PIRES MOCELIN MORAES" class="mx-auto d-block">
 		</a>
