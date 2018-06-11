6e8003dc2c5bf4263aadc947e32a91fd908cbeb8
Luiz <luizschinemann@gmail.com>
2018-06-11 20:58:34 -0300

    cabecalho fernando mocelin

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -20,7 +20,7 @@
 	<div class="row justify-content-md-center mt-4 mb-4">					
 		<div class="col-md-auto">
 			<a href="{{ url('rodrigo-correa-nunes') }}">
-				a<img src="{{asset('public/img/user.png')}}" alt="RODRIGO CORREA NUNES" class="mx-auto d-block">
+				<img src="{{asset('public/img/user.png')}}" alt="RODRIGO CORREA NUNES" class="mx-auto d-block">
 			</a>
 			<br>
 			<h4>Rodrigo Correa Nunes</h4>
--- resources/views/equipe/correa.blade.php
+++ resources/views/equipe/correa.blade.php
@@ -1,13 +1,16 @@
 @extends('layout.app')
 @section('content')
-
+<div class="headerEquipe d-flex">
+	<div class="h1 align-middle align-items-center d-flex justify-content-center" style="text-shadow: 2px 2px 4px #6d6c6c;width:100%"><center>Rodrigo Correa Nunes</center></div>	
+</div>
 <div class="container">
+
 	<div class="mt-4 mb-4 row">
 		<div class="mr-2 col-3">
-			<img src="{{asset('public/img/user.png')}}" alt="Rodrigo Correa Nunes" class="mx-auto d-block">
+			<img src="{{asset('public/img/user.png')}}" alt="FERNANDO HENRIQUE PIRES MOCELIN MORAES" class="mx-auto d-block">
 		</div>
 		<div class="col-8">
-				<p>Acadêmico do curso de Direito da UniCuritiba.</p>
+		<p>Acadêmico do curso de Direito da UniCuritiba.</p>
 		</div>		
 	</div>
 </div>
