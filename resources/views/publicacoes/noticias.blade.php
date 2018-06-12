47ec7782fa9b71447d43b6fc4ee474da3669a398
Luiz <luizschinemann@gmail.com>
2018-06-11 22:19:14 -0300

    cabecalho fernando mocelin

--- resources/views/publicacoes/noticias.blade.php
+++ resources/views/publicacoes/noticias.blade.php
@@ -3,17 +3,45 @@
 
 <div class="container mt-4 mb-4">
     <div class="row">
-        <div class="col">
+        <div class="col text-center">
             <a href="https://economia.uol.com.br/noticias/redacao/2018/06/07/oit-analisa-hoje-se-brasil-preserva-acordos-coletivos-com-trabalhador.htm">
             <img src="{{asset('public/img/artigos/noticia.jpg')}}" class="rounded mx-auto d-block">
             <span>OIT analisa hoje se Brasil preserva acordos coletivos com trabalhador</span>
             </a>
         </div>
         
-        <div class="col">
-            <a href="{{asset('public/files/O entrelace entre o Direito do Consumidor e os contratos de planos de saude.docx')}}https://economia.estadao.com.br/blogs/o-seguro-morreu-de-velho/doencas-do-trabalho-devem-ser-indenizadas-pelo-empregador/">
-            <img src="{{asset('public/img/artigos/artigo2.jpg')}}" class="rounded mx-auto d-block" alt="">
+        <div class="col text-center">
+            <a href="https://economia.estadao.com.br/blogs/o-seguro-morreu-de-velho/doencas-do-trabalho-devem-ser-indenizadas-pelo-empregador/">
+            <img src="{{asset('public/img/artigos/noticia.jpg')}}" class="rounded mx-auto d-block" alt="">
             <span>Doenças do Trabalho devem ser indenizadas pelo empregador</span>
+            </a>
+        </div>
+
+        <div class="col text-center">
+            <a href="https://www12.senado.leg.br/noticias/materias/2018/06/06/aprovada-isencao-de-carencia-para-atendimento-de-urgencia-por-planos-de-saude">
+            <img src="{{asset('public/img/artigos/noticia.jpg')}}" class="rounded mx-auto d-block" alt="">
+            <span>Aprovada isenção de carência para atendimento de urgência por planos de saúde</span>
+            </a>
+        </div>
+
+        <div class="col text-center">
+            <a href="http://www.femipa.org.br/noticias/juizes-negam-pedidos-de-medicamentos-para-evitar-impacto-nos-cofres-publicos/">
+            <img src="{{asset('public/img/artigos/noticia.jpg')}}" class="rounded mx-auto d-block" alt="">
+            <span>Juízes negam pedidos de medicamentos para evitar impacto nos cofres públicos</span>
+            </a>
+        </div>
+
+        <div class="col text-center">
+            <a href="http://www.stj.jus.br/sites/STJ/default/pt_BR/Comunica%C3%A7%C3%A3o/noticias/Not%C3%ADcias/M%C3%A9dico-que-fez-vasectomia-em-vez-de-cirurgia-de-fimose-ter%C3%A1-de-indenizar-paciente">
+            <img src="{{asset('public/img/artigos/noticia.jpg')}}" class="rounded mx-auto d-block" alt="">
+            <span>Médico que fez vasectomia em vez de cirurgia de fimose terá de indenizar paciente</span>
+            </a>
+        </div>
+
+        <div class="col text-center">
+            <a href="http://www.stj.jus.br/sites/STJ/default/pt_BR/Comunica%C3%A7%C3%A3o/noticias/Not%C3%ADcias/Herdeiro-pode-pleitear-usucapi%C3%A3o-extraordin%C3%A1ria-de-im%C3%B3vel-objeto-de-heran%C3%A7a">
+            <img src="{{asset('public/img/artigos/noticia.jpg')}}" class="rounded mx-auto d-block" alt="">
+            <span>Herdeiro pode pleitear usucapião extraordinária de imóvel objeto de herança</span>
             </a>
         </div>
         
