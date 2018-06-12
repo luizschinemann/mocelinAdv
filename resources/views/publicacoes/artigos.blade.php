b6c694e0bcfe9b0dbbfc3b6e3259c972b833543b
Luiz <luizschinemann@gmail.com>
2018-06-11 22:04:23 -0300

    cabecalho fernando mocelin

--- resources/views/publicacoes/artigos.blade.php
+++ resources/views/publicacoes/artigos.blade.php
@@ -4,10 +4,14 @@
 <div class="container mt-4 mb-4">
     <div class="row">
         <div class="col">
-        <img src="{{asset('public/img/artigos/artigo2.jpg')}}" class="rounded mx-auto d-block" alt="...">
+        <a href="{{asset('public/files/A crise brasileira e a atuação do Estado sob o enfoque do Estado')}}">
+        <img src="{{asset('public/img/artigos/artigo2.jpg')}}" class="rounded mx-auto d-block" alt="A crise brasileira e a atuação do Estado sob o enfoque do Estado">
+            <span>A crise brasileira e a atuação do Estado sob o enfoque do Estado</span>
         </div>
+        </a>
         <div class="col">
-        <img src="{{asset('public/img/artigos/artigo2.jpg')}}" class="rounded mx-auto d-block" alt="...">
+        <img src="{{asset('public/img/artigos/artigo2.jpg')}}" class="rounded mx-auto d-block" alt="O entrelace entre o Direito do Consumidor e os contratos de planos de saude">
+            <span>O entrelace entre o Direito do Consumidor e os contratos de planos de saúde</span>
         </div>
         <div class="col"></div>
         <div class="col"></div>
