b24a20b520881a1dde84839f255b058e1068cba7
Luiz <luizschinemann@gmail.com>
2018-06-11 21:57:11 -0300

    cabecalho fernando mocelin

--- resources/views/publicacoes/artigos.blade.php
+++ resources/views/publicacoes/artigos.blade.php
@@ -4,10 +4,10 @@
 <div class="container mt-4 mb-4">
     <div class="row">
         <div class="col">
-        <img src="{{asset('public/img/artigos/artigo.jpg')}}" class="rounded mx-auto d-block" alt="...">
+        <img src="{{asset('public/img/artigos/artigo2.jpg')}}" class="rounded mx-auto d-block" alt="...">
         </div>
         <div class="col">
-        <img src="{{asset('public/img/artigos/artigo.jpg')}}" class="rounded mx-auto d-block" alt="...">
+        <img src="{{asset('public/img/artigos/artigo2.jpg')}}" class="rounded mx-auto d-block" alt="...">
         </div>
         <div class="col"></div>
         <div class="col"></div>
