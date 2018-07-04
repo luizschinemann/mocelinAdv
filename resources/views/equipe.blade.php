35bf259c4e7d74b458a50b8b91ab98a3ae19bd6f
Luiz <luizschinemann@gmail.com>
2018-07-03 22:01:34 -0300

    imagens

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -32,7 +32,7 @@
 	<div class="row justify-content-md-center mt-4 mb-4">					
 		<div class="col-md-auto">
 			<a href="{{ url('rodrigo-correa-nunes') }}">
-				<img src="{{asset('public/img/user.png')}}" alt="RODRIGO CORREA NUNES" class="mx-auto d-block">
+				<img src="{{asset('public/img/equipe/rodrigo_correa.jpg')}}" alt="RODRIGO CORREA NUNES" class="mx-auto d-block">
 			</a>
 			<br>
 			<h4>Rodrigo Correa Nunes</h4>
