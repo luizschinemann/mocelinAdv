102c668fd50ef7c86a8ba619c07bbb3865b34524
Luiz <luizschinemann@gmail.com>
2018-07-03 21:27:57 -0300

    imagens

--- resources/views/publicacoes/artigos.blade.php
+++ resources/views/publicacoes/artigos.blade.php
@@ -5,13 +5,13 @@
     <div class="row">
         <div class="col">
         <a href="{{asset('public/files/A crise brasileira e a atuação do Estado sob o enfoque do Estado.docx')}}">
-        <img src="{{asset('public/img/artigos/artigo2.jpg')}}" class="rounded mx-auto d-block" alt="A crise brasileira e a atuação do Estado sob o enfoque do Estado">
+        <img src="{{asset('public/img/artigos/crise_brasileira.jpg')}}" class="rounded mx-auto d-block" alt="A crise brasileira e a atuação do Estado sob o enfoque do Estado">
             <span>A crise brasileira e a atuação do Estado sob o enfoque do Estado</span>
         </div>
         </a>
         <div class="col">
         <a href="{{asset('public/files/O entrelace entre o Direito do Consumidor e os contratos de planos de saude.docx')}}">
-        <img src="{{asset('public/img/artigos/artigo2.jpg')}}" class="rounded mx-auto d-block" alt="O entrelace entre o Direito do Consumidor e os contratos de planos de saude">
+        <img src="{{asset('public/img/artigos/defesa_consumidor.jpg')}}" class="rounded mx-auto d-block" alt="O entrelace entre o Direito do Consumidor e os contratos de planos de saude">
             <span>O entrelace entre o Direito do Consumidor e os contratos de planos de saúde</span>
         </div>
         </a>
