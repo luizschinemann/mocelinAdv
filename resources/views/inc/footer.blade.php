7f634ed5e74f4c81d043dbadd0f91d3df2438392
Luiz Schinemann <luizschinemann@gmail.com>
2018-07-24 13:30:44 -0300

    Modificando banner

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -12,6 +12,13 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 <script>
+
+$( document ).ready(function() {
+  $('.carousel').carousel({
+    interval: 100
+  })
+});
+
   window.onscroll = function() {myFunction()};
   var navbar = document.getElementById("navbar");
   var sticky = navbar.offsetTop;
