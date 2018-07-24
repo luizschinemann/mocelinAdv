cbfdcd7d688733e1641d8d3aa463cbaeff27244c
Luiz Schinemann <luizschinemann@gmail.com>
2018-07-24 14:02:20 -0300

    Modificando banner

--- resources/views/equipe/mocelin.blade.php
+++ resources/views/equipe/mocelin.blade.php
@@ -4,15 +4,16 @@
 	<div class="h1 align-middle align-items-center d-flex justify-content-center" style="text-shadow: 2px 2px 4px #6d6c6c;width:100%"><center>Fernando Henrique Pires Mocelin Moraes</center></div>	
 </div>
 <div class="container">
-
 	<div class="mt-4 mb-4 row">
 		<div class="mr-2 col-3">
 			<img src="{{asset('public/img/equipe/fernando_mocelin.jpg')}}" alt="FERNANDO HENRIQUE PIRES MOCELIN MORAES" class="mx-auto d-block">
 		</div>
-		<div class="col-8">
+		<div class="col-7">
 				<p>Advogado especializado em Direito Empresarial. Pós-graduado em MBA Executivo em Direito Empresarial pela Fundação Getúlio Vargas (FGV 2016-2017). Cursando Pós-graduação em Direito do Trabalho pela Pontifícia Universidade Católica do Rio Grande do Sul (2018-2019). É participante ativo do Grupo de Núcleos de Estudos Avançados de Direito do Trabalho e Socioeconômico, desde 2014 (coordenador: Prof. Dr. Marco Antônio César Villatore). É graduado em Direito pela Pontifícia Universidade Católica do Paraná (2011-2016).  É sócio-fundador do escritório Fernando Mocelin Moraes & Advogados. Atua no contencioso e consultivo das áreas de Direito Civil, Trabalhista e Empresarial. Forte atuação na área do Direito Médico e do Direito do Consumidor.</p>
-				
 		</div>		
+		<div class="col-3">
+			
+		</div>
 	</div>
 </div>
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{config('app.locale')}}">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <head>
         <title>Fernando Mocelin Moraes & Advogados</title>
     <link rel="stylesheet" href="{{asset('public/css/app.css')}}" type="text/css">
